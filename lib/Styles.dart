0c27926437a1df7634f1d299a2be03c2093bd17c
Lucas Perussi <perussilucas@hotmail.com>
2023-08-24 16:10:10 -0300

    Update Styles.dart

--- lib/Styles.dart
+++ lib/Styles.dart
@@ -9,9 +9,7 @@ class AppStyles {
   static const double defaultPadding = 16.0;
   static const EdgeInsetsGeometry defaultPaddingEdgeInsets =
       EdgeInsets.all(defaultPadding);
-
-  // Você pode adicionar mais constantes de estilo aqui conforme necessário
-
+  
   static const EdgeInsetsGeometry customPaddingEdgeInsets =
       EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0);
 }
